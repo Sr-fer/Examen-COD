8fe3d0408de6f8f5255b4089ea59b0f9698ac874
Fernando <ffernandezmartinez@danielcastelao.org>
2021-12-14 20:29:54 +0100

    Commit con correciones de otros apartadoos, más variables con su corresponidente nombre, mejora de espacios en variables y funciones y el style realizado

--- ex1-COD-DAW-main/funciones.js
+++ ex1-COD-DAW-main/funciones.js
@@ -1,27 +1,40 @@
-function paso_de_temperaturas(grados) {
-var operacon_paso_temperaturas = 5/9*(grados - 32);
+//@ts-check
+/**
+ * Temperaturas
+ * Temperaturas Celsius y Farenhaid
+ * @param {number}  grados - Grados con los que se quiere operar
+ * @var {number}  operacon_paso_temperaturas - operacion para el paso de temperaturas
+ * @returns {number}  operacon_paso_temperaturas - Un numero
+ * @var {number} grados_farenhaid - grados en farenhaid
+ * @var {number} grados_celsius - grados en celsius
+ * @var {number} contador - recuento de los grados
+ * @var {number} temperatura - muestra por pantalla una serie de temperaturas para decir si hace frio o calor
+ */
+
+function paso_de_temperaturas (grados) {
+var operacon_paso_temperaturas = 5 / 9 * ( grados - 32 );
 return operacon_paso_temperaturas;
 }
 
-var grados_farenhaid = [60, 32, 100];var grados_celsius = []; var contador;
+var grados_farenhaid = [60, 32, 100]; var grados_celsius = []; var contador;
 
-function contador_temperaturas() {
-for(contador = 2; contador >= 0; contador--) {
-document.write("Valor " + contador +" es " + grados_farenhaid[contador] +" F ");
+function contador_temperaturas () {
+for (contador = 2; contador >= 0; contador--) {
+document.write ("Valor " + contador +" es " + grados_farenhaid[contador] +" F ");
 
-document.write(" el cual es " + grados_celsius[contador] + " C");
+document.write (" el cual es " + grados_celsius[contador] + " C");
 
-contador_celsius(grados_celsius[contador])
+contador_celsius (grados_celsius[contador])
 }
 }
 
-function contador_diferencia_temperaturas() {for (contador = 0; contador <= 2; contador++) {grados_celsius[contador] = paso_de_temperaturas(grados_farenhaid[contador]);}
+function contador_diferencia_temperaturas () {for (contador = 0; contador <= 2; contador++) {grados_celsius[contador] = paso_de_temperaturas(grados_farenhaid[contador]);}
 }
 
-function contador_celsius(aux) {
-if (aux > 25) {document.write(" Hace calor <br />"); } 
+function contador_celsius (temperatura) {
+if ( temperatura > 25 ) { document.write(" Hace calor <br />" ); } 
 else
-if (aux < 15) {
-document.write("Hace frio <br />"); } 
+if ( temperatura < 15 ) {
+document.write ( "Hace frio <br />" ); } 
 else {
-document.write("Ni fu ni fa <br />") } }
+document.write ( "Ni fu ni fa <br />" ) } }
